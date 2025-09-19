0c7f48a5eb45f4d51e5dc4772a5d2fd10a6e38e2
HenriqueSigwalt <140031473+HenriqueSigwalt@users.noreply.github.com>
2025-09-19 08:58:54 -0300

    Controlador Dahlin

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,19 @@
 char buffer[MAX_BUFFER];
 byte indexBuffer = 0;
 
-// Forward declaration
-void processarComando(char *cmd);
+float y=0;
+float u=0;
+int rho=5;
+float P1=rho;
+float P2=0;
+float P3=0;
+float P4=rho;
+float a=0;
+float b=0;
+float r=15;
+float k=1;
+float eant=r;
+float uant=0;
 
 void setup() {
   Serial.begin(115200);
@@ -19,8 +30,6 @@ void setup() {
   // Configura PWM
   ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
   ledcAttachPin(PWM_PIN, PWM_CHANNEL);
-
-  Serial.println("ESP32 pronto para comunicação!");
 }
 
 void loop() {
@@ -29,30 +38,36 @@ void loop() {
   Serial.print("TEMP:");
   Serial.println(temp, 2);
 
-  // Recebe dados byte a byte
-  while (Serial.available() > 0) {
-    char c = Serial.read();
+  float h1=(u*P1-y*P2)/(1+((u*u)*P1-u*y*P3-u*y*P2+(y*y)*P4));
+  float h2=(u*P3-y*P4)/(1+((u*u)*P1-u*y*P3-u*y*P2+(y*y)*P4));
+  
+  a=a+h1*temp-u*a*h1+y*b*h1;
+  b=b+h2*temp-u*a*h2+y*b*h2;
 
-    if (c == '\n') {  
-      buffer[indexBuffer] = '\0';  // Fecha string
-      processarComando(buffer);    
-      indexBuffer = 0;             
-    } else {
-      if (indexBuffer < MAX_BUFFER - 1) {
-        buffer[indexBuffer++] = c; 
-      }
-    }
+  float P1temp=P1;
+  float P2temp=P2;
+  P1=P1-P1*h1*u+P3*h1*y;
+  P2=P2-P2*h1*u+P4*h1*y;
+  P3=-h2*u*P1temp+(1+h2*y)*P3;
+  P4=-h2*u*P2temp+(1+h2*y)*P4;
+
+  float e=r-temp;
+
+  float tau=1/(2*log(-a));
+  float c=-exp(-1/tau);
+
+  u=((k+k*c)*e+(k*c*a+k*a)*eant-b*c*u+(k*b+k*c*b)*uant)/b;
+
+  if(u>255){
+    u=255;
+  }
+  else if(u<0){
+    u=0;
   }
 
-  delay(2000);
-}
+  eant=e;
+  uant=u;
+  y=temp;
 
-void processarComando(char *cmd) {
-  if (strncmp(cmd, "SET_PWM:", 8) == 0) {
-    int duty = atoi(cmd + 8);  // Converte número após "SET_PWM:"
-    duty = constrain(duty, 0, 1023);
-    ledcWrite(PWM_CHANNEL, duty);
-    Serial.print("PWM atualizado para: ");
-    Serial.println(duty);
-  }
+  ledcWrite(PWM_PIN,u);
 }
