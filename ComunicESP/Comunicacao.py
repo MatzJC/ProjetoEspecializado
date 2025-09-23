ba5d23f048ce7f7c39ee3066764d3b84ced66c79
JulioMatz <jcmatzlima@gmail.com>
2025-09-22 21:30:56 -0300

    Melhorias e adição da funcionalidade de amostragem dos resultados

--- ComunicESP/Comunicacao.py
+++ ComunicESP/Comunicacao.py
@@ -6,25 +6,36 @@ porta_serial = 'COM3'
 baudrate = 115200      # ESP32 agora roda estável em 115200
 
 referencia = 5.0  # °C desejada(trocar para o valor ser fornecido pelo usuário)
-Kp = 40           # ganho proporcional
 
 try:
     ser = serial.Serial(porta_serial, baudrate, timeout=1)
     time.sleep(2)  # tempo para estabilizar a conexão
     print("Conexão serial estabelecida com o ESP32.")
 
-    # 🔹 Descarta qualquer dado que ainda esteja no buffer
+    # Descarta qualquer dado que ainda esteja no buffer
     ser.reset_input_buffer()
 
     while True:
-        if ser.in_waiting > 0:
-            try:
-                porta_serial.write(referencia.encode())
-                print(f'Referência enviada: {referencia}°C')
-            except KeyboardInterrupt:
-                print("Comunicação encerrada.")
-                porta_serial.close() # Fecha a conexão serial
-        time.sleep(0.5)
+        try:
+            ser.write(f"{referencia}\n".encode())
+            print(f'Referência enviada: {referencia}°C')
+            if ser.in_waiting > 0:
+                try:
+                    linha = ser.readline().decode().strip()
+                    if linha:
+                        temperatura_atual = float(linha)
+                        print(f'Temperatura atual do sistema: {temperatura_atual}°C')
+                        erro = referencia - temperatura_atual
+                        print(f'Erro: {erro}°C')
+                    else:
+                        print('Nenhum dado recebido.')
+                except ValueError:
+                    print('Erro ao converter a temperatura recebida.')
+            time.sleep(0.5)
+
+        except KeyboardInterrupt:
+            print("Comunicação encerrada pelo usuário.")
+            break
 
 except serial.SerialException as e:
     print(f"Erro ao conectar na porta serial: {e}")
